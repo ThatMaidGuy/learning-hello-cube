2fdefdc24a8e62dd11a8d984595e4d5240161ee4
Dmitriy Volkov <37198050+ThatMaidGuy@users.noreply.github.com>
2026-03-14 11:13:02 +0500

    added cube and script with hello world

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("HelloWorld");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(this);
+        }
+    }
+}
